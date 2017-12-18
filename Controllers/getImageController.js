39e9e2f6b80d23fb8e4c76d3a2e0e768b8204c0c
LAPTOP-7QQ4KURV\Sangameswaran <blackdrafteaglekrish@gmail.com>
2017-12-18 23:35:58 +0530

    Mindfucked

---
+++ Controllers/getImageController.js
@@ -0,0 +1,5 @@
+(function () {
+    module.exports.getImages=function (req,res) {
+        res.sendFile("C://Users/Sangameswaran/Downloads/blood_drop_icon.png");
+    }
+})();
--- availableRoutes.js
+++ availableRoutes.js
@@ -1,8 +1,42 @@
 (function () {
     var express=require('express');
     var Router=express.Router();
+    var getImageController=require('./Controllers/getImageController');
+    var multer = require('multer');
+    var Storage = multer.diskStorage({
+        destination: function(req, file, callback) {
+            callback(null, "C://Users/Sangameswaran/profPics/");
+        },
+        filename: function(req, file, callback) {
+            callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+        }
+    });
+    var upload = multer({
+        storage: Storage
+    }).array("imgUploader", 3);
     Router.get('/testConn',function (req,res) {
        res.send('Connection OK');
+    });
+    Router.get('/getFileTest',function (req,res) {
+        getImageController.getImages(req,res);
+    });
+    Router.post('/uploadFileTest',function (req,res) {
+        upload(req, res, function(err) {
+            if (err) {
+                console.log(err);
+                var errorJson={
+                  statusCode : 500,
+                    message : 'Error Uploading file! Try Again !!'
+                };
+                return res.status(500).send(errorJson);
+            }else {
+                var successJson={
+                  statusCode : 200,
+                    message : 'Image uploaded successfully'
+                };
+                return res.send(successJson);
+            }
+        });
     });
     module.exports=Router;
 })();
