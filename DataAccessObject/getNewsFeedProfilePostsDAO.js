e7c16c4744a9e1f7f2d4d8b87a7c2f393ba843d0
LAPTOP-7QQ4KURV\Sangameswaran <blackdrafteaglekrish@gmail.com>
2018-02-11 00:11:06 +0530

    Bugs

--- DataAccessObject/getNewsFeedProfilePostsDAO.js
+++ DataAccessObject/getNewsFeedProfilePostsDAO.js
@@ -1,7 +1,7 @@
 (function () {
     var connection=require('../Connection/connection');
     module.exports.getDataFromTable=function (userId,callBack) {
-        connection.query('select postinfo.postId,postInfo.postContent,postInfo.likeCount,postInfo.commentCount,postInfo.imageUrl,postInfo.postTime,usertopostmap.userId from postinfo inner join usertopostmap on postinfo.postId = usertopostmap.postId and usertopostmap.userId in (select followingUserId as userId from usertofollowingmap where userId=?);',userId,function (err,data) {
+        connection.query('select user.userName,postinfo.postId,postInfo.postContent,postInfo.likeCount,postInfo.commentCount,postInfo.imageUrl,postInfo.postTime,usertopostmap.userId from postinfo inner join usertopostmap on postinfo.postId = usertopostmap.postId inner join user on usertopostmap.userId= user.userId and usertopostmap.userId in (select followingUserId as userId from usertofollowingmap where userId=?) order by postinfo.postTime desc;',userId,function (err,data) {
            if(err){
                callBack(err,null);
            } else{
