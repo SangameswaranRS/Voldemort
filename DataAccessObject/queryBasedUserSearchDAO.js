a4b17b09123e738bbe4450d26ccefcaa2285ead1
LAPTOP-7QQ4KURV\Sangameswaran <blackdrafteaglekrish@gmail.com>
2018-03-15 14:55:36 +0530

    Bug Fixes

--- DataAccessObject/queryBasedUserSearchDAO.js
+++ DataAccessObject/queryBasedUserSearchDAO.js
@@ -2,7 +2,7 @@
     var connection = require('../Connection/connection');
     module.exports.queryBasedUserSearch = function (query,callBack) {
         var searchString ='%'+query+'%';
-      connection.query('select * from user where userName like ?',searchString,function (err,data) {
+      connection.query('select userId,userName from user where userName like ?',searchString,function (err,data) {
         callBack(err,data);
       });
     };
